4233b579f33925858ecb5f2049b7fd6721c6c136
Franklin Guevara <122811251+FranklinGuevaraFlorian@users.noreply.github.com>
2024-03-13 19:05:33 -0500

    project structure

---
+++ CashControl/src/test/java/com/back/CashControl/CashControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.CashControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
